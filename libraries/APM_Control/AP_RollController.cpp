1523335ac1fedf57bbd280804a206b71c5dc5f07
NCKUdent <44294628+NCKUdent@users.noreply.github.com>
2019-12-22 22:43:21 +0800

    Add files via upload

--- libraries/APM_Control/AP_RollController.cpp
+++ libraries/APM_Control/AP_RollController.cpp
@@ -233,7 +233,7 @@ int32_t AP_RollController::_custom_get_rate_out(float desired_rate, bool disable
 	float inner_P = 0.07571086;
 	float inner_I = 0.99674439;
 	float inner_D = 0.00143771;
-	float delta_time = (float)dt * 0.001f;
+	float delta_time = (float)dt * 0.001f * 40;
 	
     // Get body rate vector (radians/sec)
 	float omega_x = _ahrs.get_gyro().x;
